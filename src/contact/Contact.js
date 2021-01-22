c6e9344aac32b8693b7c4555ff2c7c743054807f
piotr-andrzej-bochenek <piotr8207@interia.eu>
2021-01-22 16:51:41 +0100

    add title to iframe in Contact

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -40,7 +40,7 @@ export default class Contact extends Component {
                 </List.Content>
             </List.Item>
         </List>
-        <iframe className='Contact__map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37116.85055112168!2d16.398235576432384!3d54.44876119926578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fe2be37b4a2dbd%3A0x971a960f1050e3b7!2sAgro-Wczasy%20Barbara%20i%20Czes%C5%82aw%20Chaber!5e0!3m2!1spl!2spl!4v1611218986500!5m2!1spl!2spl" frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+        <iframe title='Cisowo 32a - wskazówki dojazdu' className='Contact__map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37116.85055112168!2d16.398235576432384!3d54.44876119926578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fe2be37b4a2dbd%3A0x971a960f1050e3b7!2sAgro-Wczasy%20Barbara%20i%20Czes%C5%82aw%20Chaber!5e0!3m2!1spl!2spl!4v1611218986500!5m2!1spl!2spl" frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
       </div>
     );
   };
