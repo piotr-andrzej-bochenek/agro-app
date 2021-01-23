f50b70b2a116c00b901c21a963316fe7ddcc6e76
piotr-andrzej-bochenek <piotr8207@interia.eu>
2021-01-23 17:35:26 +0100

    03_Content export carousel & images to separate components in Surroundings

--- src/surroundings/CarouselSurroundings.js
+++ src/surroundings/CarouselSurroundings.js
@@ -4,26 +4,24 @@ import { CarouselProvider, Image, Slide, Slider } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
 
 import CarouselDotGroup from '../components/CarouselDotGroup';
-import img01 from '../assets/photos/rooms/pokoje_01.jpg';
-import img02 from '../assets/photos/rooms/pokoje_02.jpg';
-import img03 from '../assets/photos/rooms/pokoje_03.jpg';
-import img04 from '../assets/photos/rooms/pokoje_04.jpg';
-import img05 from '../assets/photos/rooms/pokoje_05.jpg';
-import img06 from '../assets/photos/rooms/pokoje_06.jpg';
-import img07 from '../assets/photos/rooms/pokoje_07.jpg';
-import img08 from '../assets/photos/rooms/pokoje_08.jpg';
-import img09 from '../assets/photos/rooms/pokoje_09.jpg';
-import img10 from '../assets/photos/rooms/pokoje_10.jpg';
-import img11 from '../assets/photos/rooms/pokoje_11.jpg';
-import img12 from '../assets/photos/rooms/pokoje_12.jpg';
-import img13 from '../assets/photos/rooms/pokoje_13.jpg';
-import img14 from '../assets/photos/rooms/pokoje_14.jpg';
-import img15 from '../assets/photos/rooms/pokoje_15.jpg';
-import img16 from '../assets/photos/rooms/pokoje_16.jpg';
-import img17 from '../assets/photos/rooms/pokoje_17.jpg';
-import img18 from '../assets/photos/rooms/pokoje_18.jpg';
+import img01 from '../assets/photos/surroundings/okol_01.jpg';
+import img02 from '../assets/photos/surroundings/okol_02.jpg';
+import img03 from '../assets/photos/surroundings/okol_03.jpg';
+import img04 from '../assets/photos/surroundings/okol_04.jpg';
+import img05 from '../assets/photos/surroundings/okol_05.jpg';
+import img06 from '../assets/photos/surroundings/okol_06.jpg';
+import img07 from '../assets/photos/surroundings/okol_07.jpg';
+import img08 from '../assets/photos/surroundings/okol_08.jpg';
+import img09 from '../assets/photos/surroundings/okol_09.jpg';
+import img10 from '../assets/photos/surroundings/okol_10.jpg';
+import img11 from '../assets/photos/surroundings/okol_11.jpg';
+import img12 from '../assets/photos/surroundings/okol_12.jpg';
+import img13 from '../assets/photos/surroundings/okol_13.jpg';
+import img14 from '../assets/photos/surroundings/okol_14.jpg';
+import img15 from '../assets/photos/surroundings/okol_15.jpg';
+import img16 from '../assets/photos/surroundings/okol_16.jpg';
 
-export default class CarouselRooms extends React.Component {
+export default class CarouselSurroundings extends React.Component {
 
     render() {
 
@@ -32,7 +30,7 @@ export default class CarouselRooms extends React.Component {
             <CarouselProvider
               naturalSlideWidth={1}
               naturalSlideHeight={1}
-              totalSlides={18}
+              totalSlides={16}
             >
               <Slider>
                 <Slide index={0}>
@@ -83,16 +81,10 @@ export default class CarouselRooms extends React.Component {
                 <Slide index={15}>
                   <Image src={img16} />
                 </Slide>
-                <Slide index={16}>
-                  <Image src={img17} />
-                </Slide>
-                <Slide index={17}>
-                  <Image src={img18} />
-                </Slide>
               </Slider>
 
               <Divider />
-              <CarouselDotGroup slides={18} />
+              <CarouselDotGroup slides={16} />
             </CarouselProvider>
           </div>
         );
