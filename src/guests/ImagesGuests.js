105f22212f68a3fc63ddeef99b9810a6079555b6
piotr-andrzej-bochenek <piotr8207@interia.eu>
2021-01-24 10:41:14 +0100

    04_FixGuests fix links to images in Guests

--- src/guests/ImagesGuests.js
+++ src/guests/ImagesGuests.js
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import { Image } from 'semantic-ui-react';
 
-import img01 from '../assets/photos/surroundings/okol_01.jpg';
-import img02 from '../assets/photos/surroundings/okol_02.jpg';
-import img03 from '../assets/photos/surroundings/okol_03.jpg';
-import img04 from '../assets/photos/surroundings/okol_04.jpg';
-import img05 from '../assets/photos/surroundings/okol_05.jpg';
-import img06 from '../assets/photos/surroundings/okol_06.jpg';
-import img07 from '../assets/photos/surroundings/okol_07.jpg';
-import img08 from '../assets/photos/surroundings/okol_08.jpg';
-import img09 from '../assets/photos/surroundings/okol_09.jpg';
-import img10 from '../assets/photos/surroundings/okol_10.jpg';
-import img11 from '../assets/photos/surroundings/okol_11.jpg';
-import img12 from '../assets/photos/surroundings/okol_12.jpg';
-import img13 from '../assets/photos/surroundings/okol_13.jpg';
-import img14 from '../assets/photos/surroundings/okol_14.jpg';
-import img15 from '../assets/photos/surroundings/okol_15.jpg';
-import img16 from '../assets/photos/surroundings/okol_16.jpg';
+import img01 from '../assets/photos/guests/goscie_01.jpg';
+import img02 from '../assets/photos/guests/goscie_02.jpg';
+import img03 from '../assets/photos/guests/goscie_03.jpg';
+import img04 from '../assets/photos/guests/goscie_04.jpg';
+import img05 from '../assets/photos/guests/goscie_05.jpg';
+import img06 from '../assets/photos/guests/goscie_06.jpg';
+import img07 from '../assets/photos/guests/goscie_07.jpg';
+import img08 from '../assets/photos/guests/goscie_08.jpg';
+import img09 from '../assets/photos/guests/goscie_09.jpg';
+import img10 from '../assets/photos/guests/goscie_10.jpg';
+import img11 from '../assets/photos/guests/goscie_11.jpg';
+import img12 from '../assets/photos/guests/goscie_12.jpg';
+import img13 from '../assets/photos/guests/goscie_13.jpg';
+import img14 from '../assets/photos/guests/goscie_14.jpg';
+import img15 from '../assets/photos/guests/goscie_15.jpg';
+import img16 from '../assets/photos/guests/goscie_16.jpg';
 
 export default class ImagesGuests extends React.Component {
 
